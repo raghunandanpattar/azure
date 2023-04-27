e4a917895a2411eb21348ed2c1bfc58c3ce95d0d
raghunandanpattar <53566456+raghunandanpattar@users.noreply.github.com>
2023-04-27 10:33:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,73 +13,73 @@ provider "azurerm" {
   subscription_id = "0cf986a3-a795-4a6e-94fd-2eeef4ddc321"
   features {}   
 }
-resource "azurerm_resource_group" "Raghunandan" {
-  name     = "Raghunandan"
+resource "azurerm_resource_group" "Raghunandan_pattar" {
+  name     = "Raghunandan_pattar"
   location = "West Europe"
   tags = {
     owner = "Raghu"
   }
 }
-resource "azurerm_storage_account" "s3bolte" {
-  name = "s3bolte"
-  resource_group_name = azurerm_resource_group.Raghunandan.name
-  location = azurerm_resource_group.Raghunandan.location
+resource "azurerm_storage_account" "storage_account_vm" {
+  name = "storage_account_vm"
+  resource_group_name = azurerm_resource_group.Raghunandan_pattar.name
+  location = azurerm_resource_group.Raghunandan_pattar.location
   account_tier = "Standard"
   account_replication_type = "LRS"
   tags ={
-    name = "storage-account"
+    name = "storage-account-vm"
   }
 }
-resource "azurerm_storage_container" "containerbolte" {
-  name = "containerbolte"
-  storage_account_name = "s3bolte"
+resource "azurerm_storage_container" "container_VM" {
+  name = "container_VM"
+  storage_account_name = "storage_account_vm"
   container_access_type = "blob"
 }
-resource "azurerm_storage_blob" "blobbolte" {
+resource "azurerm_storage_blob" "blob_VM" {
   name                   = "blobbolte.txt"
-  storage_account_name   = azurerm_storage_account.s3bolte.name
-  storage_container_name = azurerm_storage_container.containerbolte.name
+  storage_account_name   = azurerm_storage_account.storage_account_vm.name
+  storage_container_name = azurerm_storage_container.container_VM.name
   type                   = "Block"
   source                 = "blobbolte.txt"
 }
-resource "azurerm_virtual_network" "vnet" {
-  name                = "raghu-network"
+resource "azurerm_virtual_network" "vnet-1" {
+  name                = "raghu-network-1"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.Raghunandan.location
-  resource_group_name = azurerm_resource_group.Raghunandan.name
+  location            = azurerm_resource_group.Raghunandan_pattar.location
+  resource_group_name = azurerm_resource_group.Raghunandan_pattar.name
 }
-resource "azurerm_subnet" "raghu-subnet" {
-  name                 = "vmsubnet"
-  resource_group_name  = azurerm_resource_group.Raghunandan.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+resource "azurerm_subnet" "raghu-subnet-1" {
+  name                 = "vmsubnet-1"
+  resource_group_name  = azurerm_resource_group.Raghunandan_pattar.name
+  virtual_network_name = azurerm_virtual_network.vnet-1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-resource "azurerm_network_interface" "nic" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.Raghunandan.location
-  resource_group_name = azurerm_resource_group.Raghunandan.name
+resource "azurerm_network_interface" "nic-1" {
+  name                = "nic-1"
+  location            = azurerm_resource_group.Raghunandan_pattar.location
+  resource_group_name = azurerm_resource_group.Raghunandan_pattar.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.raghu-subnet.id
+    subnet_id                     = azurerm_subnet.raghu-subnet-1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.public_ip.id
     
   }
   depends_on = [
-    azurerm_virtual_network.vnet,
+    azurerm_virtual_network.vnet-1,
     azurerm_public_ip.public_ip
   ]
 }
-resource "azurerm_windows_virtual_machine" "wvm" {
-  name                = "wvm"
-  resource_group_name = azurerm_resource_group.Raghunandan.name
-  location            = azurerm_resource_group.Raghunandan.location
+resource "azurerm_linux_virtual_machine" "windows_vm" {
+  name                = "windows_vm"
+  resource_group_name = azurerm_resource_group.Raghunandan_pattar.name
+  location            = azurerm_resource_group.Raghunandan_pattar.location
   size                = "Standard_F2"
   admin_username      = "raghu"
   admin_password      = "Raghupattar_2504"
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.nic-1.id,
   ]
 
   os_disk {
@@ -88,9 +88,9 @@ resource "azurerm_windows_virtual_machine" "wvm" {
   }
 
   source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
@@ -98,12 +98,10 @@ resource "azurerm_public_ip" "public_ip" {
 
 name = "public_ip"
 
-location = azurerm_resource_group.Raghunandan.location
+location = azurerm_resource_group.Raghunandan_pattar.location
 
-resource_group_name = azurerm_resource_group.Raghunandan.name
+resource_group_name = azurerm_resource_group.Raghunandan_pattar.name
 
 allocation_method = "Static"
-
-
 
 }
